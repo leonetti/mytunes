3000796cfffb5cf45d9160b46466244986da723f
leonetti <leonetti.tech@gmail.com>
2015-07-16 11:40:59 -0700

    Successful Merge

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -10,34 +10,16 @@ var SongQueue = Songs.extend({
       this.remove(this.at(0));
     });
 
-<<<<<<< HEAD
-    this.on('enqueue', function() {
+    this.on('add', function() {
       if(this.length === 1) {
         this.playFirst();
       }
     });
-
-   
-
-    // this.on('play', function(){
-    //   this.playFirst();
-    // });
-=======
-    // this.on('enqueue', function(song){
-    //   console.log("hey")
-    // });
-
->>>>>>> c0bbec45d00cacc54a8f4cc996475b3c6266aa11
   },
 
 
   playFirst: function() {
-<<<<<<< HEAD
     this.at(0).play();
-=======
-      // console.log(this.at(0));
-      this.at(0).play();
->>>>>>> c0bbec45d00cacc54a8f4cc996475b3c6266aa11
   }
 
 });
